045b19a817d947d59e8045a2011d720b34591996
RobloxReal1 <164631113+RobloxReal1@users.noreply.github.com>
2024-04-01 20:30:41 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,87 @@
+from multiprocessing import Process, Value, Array, cpu_count, Lock
+from statistics import fmean, StatisticsError
+from requests import Session
+from random import randint
+import time
+
+session = Session()
+
+# number of processes that are started
+count = 30000
+# uncomment for whathow (main counter)
+url, session.headers.update = "http://counter11.freecounterstat.com/private/counter.php?c=pdz4dufhlf9qlk4krksnw7twxbhlez2e&init=1711160585473", ({'referer': "https://www.youtube.com/watch?v=mH7EFJrIwZY/"})
+# uncomment for jared site
+# url, session.headers.update = "http://www.cutercounter.com/hits.php?id=hexpacno&nd=6&style=61", ({'referer': "https://jared.nekoweb.org/"})
+
+def process(counter, fails, sysfails, avg_time, child, lock):
+    while True:
+        try:
+            # gets response time and status code, then closes request
+            start = time.time()
+            response = session.head(url)
+            rtime = time.time() - start
+            status=response.status_code
+            response.close()
+
+            # if successful, increment success counter and randomly add response time to average array
+            if (response.status_code == 200):
+                with lock:
+                    counter.value += 1
+                    avg_time[randint(0, 49)] = rtime
+                    None
+            # if failure, increment fail counter and randomly add response time
+            elif (response.status_code != 200):
+                with lock:
+                    fails.value += 1
+                    avg_time[randint(0, 49)] = rtime
+                None
+        # if exception, increment sysfails and print error
+        except Exception as e:
+            with lock:
+                print(e)
+                sysfails.value += 1
+            None
+
+def main():
+    # initialize multiprocessing variables
+    counter = Value('L', 0)
+    fails = Value('L', 0)
+    sysfails = Value('L', 0)
+    avg_time = Array('f', [1] * 50)
+    lock = Lock()
+    # array of processes
+    children = []
+    # elapsed time timer
+    main_start = time.time()  
+
+    # create [count] processes
+    for i in range(count):
+        children.append(Process(target=process, args=(counter, fails, sysfails, avg_time, url, lock)))
+        children[i].start()
+        print((f"{(i / (count - 1) * 100):.2f}% of processes started."), end='\r')
+    
+    # workaround so the previous printing doesn't get overwritten 
+    print("100% of processes started.")
+    print("Ctrl+C to end")
+    while True:
+        # printing status, '\r' is to update newest line
+        print((f"| {(time.time() - main_start):.2f} seconds elapsed"),
+              (f"| {counter.value} succeeded"),
+              (f"| {fails.value} failed"),
+              (f"| {sysfails.value} errors"),
+              (f"| average ping: {fmean(avg_time[:4]):.3f} |"), end='\r')
+
+        time.sleep(0.08)
+
+# doesn't run if imported
+if (__name__ == "__main__"):
+    try:
+        main()
+    # handles ctrl c without leaving orphans now
+    except (KeyboardInterrupt, SystemExit):
+        for child in children:
+            child.terminate()
+        print("you killed it :(")
+    except Exception as e:
+        print(e)
+        print("it died :(")
